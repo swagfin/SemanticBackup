3fa35ef34d839039caa1bce40ee195f04ed8cee7
George Wainaina Njeri <george@pixsel.net>
2021-11-30 19:58:46 +0300

    Subscribbing Completion

--- SemanticBackup.WebClient/Pages/DatabaseBackups/Index.cshtml.cs
+++ SemanticBackup.WebClient/Pages/DatabaseBackups/Index.cshtml.cs
@@ -1,17 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SemanticBackup.WebClient.Models.Response;
+using SemanticBackup.WebClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SemanticBackup.WebClient.Pages.DatabaseBackups
 {
     public class IndexModel : PageModel
     {
         public string ApiEndPoint { get; }
-        public IndexModel(IOptions<WebClientOptions> options)
+
+        private readonly IHttpService _httpService;
+        private readonly ILogger<IndexModel> _logger;
+        public List<BackupRecordResponse> BackupRecordsResponse { get; set; }
+        public IndexModel(IOptions<WebClientOptions> options, IHttpService httpService, ILogger<IndexModel> logger)
         {
+            this._httpService = httpService;
+            this._logger = logger;
             ApiEndPoint = options.Value.WebApiUrl;
         }
-        public void OnGet()
+
+        public async Task<IActionResult> OnGetAsync()
         {
+            try
+            {
+                var url = "api/BackupRecords/";
+                BackupRecordsResponse = await _httpService.GetAsync<List<BackupRecordResponse>>(url);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+            return Page();
         }
     }
 }
