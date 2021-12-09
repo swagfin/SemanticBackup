848b8683398c944d5d8f2efad7cecd1acb4b7e4a
George Wainaina Njeri <georgewainaina18@gmail.com>
2021-12-09 18:59:58 +0300

    Added Auto Complete for Database Names in Database Registration

--- SemanticBackup.Core/ProviderServices/IMySQLServerBackupProviderService.cs
+++ SemanticBackup.Core/ProviderServices/IMySQLServerBackupProviderService.cs
@@ -1,9 +1,12 @@
 ﻿using SemanticBackup.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SemanticBackup.Core.ProviderServices
 {
     public interface IMySQLServerBackupProviderService
     {
         bool BackupDatabase(BackupDatabaseInfo backupDatabaseInfo, BackupRecord backupRecord);
+        Task<IEnumerable<string>> GetAvailableDatabaseCollectionAsync(BackupDatabaseInfo backupDatabaseInfo);
     }
 }
--- SemanticBackup.WebClient/Pages/Databases/RegisterDatabase.cshtml.cs
+++ SemanticBackup.WebClient/Pages/Databases/RegisterDatabase.cshtml.cs
@@ -5,6 +5,8 @@ using SemanticBackup.WebClient.Models.Requests;
 using SemanticBackup.WebClient.Models.Response;
 using SemanticBackup.WebClient.Services;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SemanticBackup.WebClient.Pages.Databases
@@ -14,12 +16,17 @@ namespace SemanticBackup.WebClient.Pages.Databases
         private readonly IHttpService _httpService;
         private readonly ILogger<IndexModel> _logger;
 
+        public string ApiEndPoint { get; }
         [BindProperty]
         public BackupDatabaseRequest backupDatabaseRequest { get; set; }
+        [BindProperty]
+        public IEnumerable<string> DatabaseNames { get; set; }
+        public string ErrorResponse { get; set; } = null;
         public RegisterDatabaseModel(IHttpService httpService, ILogger<IndexModel> logger)
         {
             this._httpService = httpService;
             this._logger = logger;
+            ApiEndPoint = Directories.CurrentDirectory?.Url;
         }
         public void OnGet()
         {
@@ -28,6 +35,27 @@ namespace SemanticBackup.WebClient.Pages.Databases
         {
             try
             {
+                ErrorResponse = null;
+                if (string.IsNullOrWhiteSpace(backupDatabaseRequest.DatabaseType))
+                {
+                    ErrorResponse = "First Select the Database Type";
+                    return Page();
+                }
+                if (string.IsNullOrWhiteSpace(backupDatabaseRequest.Server))
+                {
+                    ErrorResponse = "Server Name was not provided";
+                    return Page();
+                }
+                if (DatabaseNames == null || DatabaseNames.Count() < 1)
+                {
+                    ErrorResponse = "Select or add atlist one Database";
+                    return Page();
+                }
+                else
+                {
+                    backupDatabaseRequest.DatabaseName = string.Join(",", DatabaseNames.Select(x => x));
+                }
+                //Proceed
                 var url = "api/BackupDatabases/";
                 var result = await _httpService.PostAsync<StatusResponseModel>(url, backupDatabaseRequest);
                 return RedirectToPage("Index");
@@ -35,6 +63,7 @@ namespace SemanticBackup.WebClient.Pages.Databases
             catch (Exception ex)
             {
                 _logger.LogError(ex.Message);
+                ErrorResponse = ex.Message;
                 return Page();
             }
 
