966d257992717560697b8521b5cab5dcde575865
Wainaina George (Swagfin) <georgewainaina18@gmail.com>
2023-09-30 01:57:00 +0300

    chore: scheduled backups pages routing

--- SemanticBackup/Pages/ResourceGroups/Delete.cshtml.cs
+++ SemanticBackup/Pages/ResourceGroups/Delete.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace SemanticBackup.Pages.ResourceGroups
 {
+    [Authorize]
     public class DeleteModel : PageModel
     {
 
