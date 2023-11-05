905ce1f85f4e637fe927cd719b32d63228dd1a59
Ronald Sigei <ronald@pixsel.net>
2023-11-05 19:58:55 +0300

    update records

--- SemanticBackup/Pages/Account/Index.cshtml.cs
+++ SemanticBackup/Pages/Account/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -5,22 +7,26 @@ using Microsoft.Extensions.Logging;
 using SemanticBackup.Core.Interfaces;
 using SemanticBackup.Core.Models;
 using SemanticBackup.Core.Models.Requests;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace SemanticBackup.Pages.Account
 {
     public class IndexModel : PageModel
     {
         private readonly IUserAccountRepository _userAccountRepository;
+        private readonly ILogger<IndexModel> _logger;
 
         [BindProperty]
         public UserAccount userAccount { get; set; }
-        public IndexModel(IUserAccountRepository userAccountRepository)
+        public IndexModel(IUserAccountRepository userAccountRepository, ILogger<IndexModel> logger)
         {
             this._userAccountRepository = userAccountRepository;
+            _logger = logger;
         }
 
         public IActionResult OnGetAsync()
@@ -29,15 +35,48 @@ namespace SemanticBackup.Pages.Account
             if (userEmailClaim != null)
             {
                 var userEmail = userEmailClaim.Value;
-                var user =  _userAccountRepository.GetByEmailAsync(userEmail).Result;
+                var user = _userAccountRepository.GetByEmailAsync(userEmail).Result;
 
                 if (user != null)
                 {
-                   userAccount = user;
+                    userAccount = user;
                 }
             }
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            try
+            {
+                returnUrl = returnUrl ?? Url.Content("~/");
+
+                var userUpdate = new UserAccount()
+                {
+                    UserAccountType = userAccount.UserAccountType,
+                    EmailAddress = userAccount.EmailAddress,
+                    FullName = userAccount.FullName,
+                    Id = userAccount.Id,
+                    Timezone = userAccount.Timezone,
+                    TimezoneOffset = userAccount.TimezoneOffset,
+                    Password = userAccount.Password,
+                    LastLoginToken = userAccount.LastLoginToken,
+                    LastLoginUTC = userAccount.LastLoginUTC,
+                };
+
+                var response = await _userAccountRepository.UpdateAsync(userUpdate);
+
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+
+            }
+            //Finnally
+            return Page();
+
         }
     }
 }
